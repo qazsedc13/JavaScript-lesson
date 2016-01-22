65aa9d496480688286938c06b5a14962a175ee95
qazsedc13 <bgnikkov@rambler.ru>
2016-01-22 12:19:51 +0600

    Попробовали создать несколько функций. Обработали данные. Полный контроль.

--- Ajax/mySite/www/index.php
+++ Ajax/mySite/www/index.php
@@ -3,9 +3,34 @@
 <head>
 <meta charset="utf-8">
 <title>Test</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script>
+	function funcBefore(){
+		$("#information").text("Ожидание данных...");
+	}
+	
+	function funcSuccess(data){
+		$("#information").text(data);
+	}
+	
+	$(document).ready(function (){
+		$("#load").bind("click", function (){
+		var admin = "Admin";
+			$.ajax({
+				url: "content.php",
+				type: "POST",
+				data: ({name: admin, number: 5}),
+				dataType:"html",
+				beforeSend: funcBefore,
+				success: funcSuccess	
+			});	
+		});	
+	});
+</script>
 </head>
 
 <body>
-<p>Всё должно работать</p>
+<p id="load" style="cursor:pointer">Загрузить данные</p>
+<div id="information"></div>
 </body>
 </html>
