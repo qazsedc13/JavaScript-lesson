3cf2b7cebef5b84f3449250c68de887506bbea45
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-23 22:02:07 +0600

    вывели переменные

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -2,5 +2,14 @@
 	$myNumber = 45;
 	$float = -3.25;
 	$string = "Hello World!";
-	$bool = true;    
+	$bool = false;
+	
+	echo "Переменная myNumber = $myNumber";// в одинарных кавычках выведется как текст, без подставновки значения переменных
+	echo "<br />"; 
+	echo "Переменная float = $float";
+	echo "<br />"; 
+	echo "Переменная string = $string";
+	echo "<br />"; 
+	echo "Переменная bool = $bool";//true выводится как 1, а false выводится как пустое место
+	echo "<br />";    
 ?>
