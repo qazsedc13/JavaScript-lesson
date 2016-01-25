73aa3af2c836f2d1cfe987ff6f3e604171081b18
qazsedc13 <bgnikkov@rambler.ru>
2016-01-25 12:40:58 +0600

    Константы в языке php

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,15 +1,6 @@
 ﻿<?php
-	$myNumber = 45;
-	$float = -3.25;
-	$string = "Hello World!";
-	$bool = false;
-	
-	echo "Переменная myNumber = $myNumber";// в одинарных кавычках выведется как текст, без подставновки значения переменных
-	echo "<br />"; 
-	echo "Переменная float = $float";
-	echo "<br />"; 
-	echo "Переменная string = $string";
-	echo "<br />"; 
-	echo "Переменная bool = $bool";//true выводится как 1, а false выводится как пустое место
-	echo "<br />";    
+	echo phpversion();
+	define("PI", 3.14); 
+	echo "<br />";
+	echo PI;   
 ?>
