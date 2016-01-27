66ef08896e0a5e175402d9574547b87690a13b91
qazsedc13 <bgnikkov@rambler.ru>
2016-01-27 13:21:08 +0600

    Рассмотрели двумерные массивы

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,30 +1,14 @@
 ﻿<?php
-	$array = array(12, -3.25, "String", false);
-	echo $array[1];	
-	$array[1] = "String";// заменили значения элементов
-	$array[2] = 4.25;
-	echo "<br />".$array[1]."<br />".$array[2];	
-	$array[] = "New element";// добавили новый элемент в конец
-	echo "<br />".$array[4]."<hr />";
+	$array = array(array(12, 4.46, "string", true), array("Example"), array(45, "strong"));
 	
-	// для вывода всего массива целиком используются циклы
+	echo $array[0][2]."<hr />";// с двумерными работают все те же функции что и с одномерными
 	
-	for ($i = 0; $i < count ($array); $i++){
-		echo "Элемент массива с индексом $i = ".$array[$i]."<br />";	
-	}
+	//Перебор элементов в двумерном массиве
 	
-	$list = array ("age" => 12, "name" => "Alex", "schoolBoy" => true); // Цикл for не применяется для ассациативных массивов
-	
-	$list["age"] = 10;
-	
-	$summa = 0;
-	
-	echo "<hr />".getAverage(array("first" => 12, "sec" => 45, "third" => 23, "forth" => 55));
-	
-	function getAverage ($array){
-		foreach ($array as $key => $value){// этот цикл используется только для ассщциативных массивов
-			$summa += $value;
-		}
-		return $summa / count ($array);
+	for ($i = 0; $i < count($array); $i++){
+		for ($j = 0; $j < count($array[$i]); $j++){
+			echo $array[$i][$j]." | ";//вывод в виде матрицы
+		}	
+		echo "<br />";
 	}
 ?>
