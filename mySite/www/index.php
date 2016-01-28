08eb9fc91d269a3f81938a2a95bc45bab9b1c9f1
qazsedc13 <bgnikkov@rambler.ru>
2016-01-28 14:11:36 +0600

    Прописали удаление сессии

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -3,5 +3,7 @@
 	$num = (isset($_SESSION["num"])) ? $_SESSION["num"] : 0;
 	$num ++;
 	$_SESSION["num"] = $num;
-	echo "Пользователь обновил страничку $num раз";	
+	echo "Пользователь обновил страничку $num раз";
+	
+	session_destroy ();	
 ?>
