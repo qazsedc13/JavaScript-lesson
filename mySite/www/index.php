7d943e00383ad6cf4c2556724583056e4981531b
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-28 20:07:38 +0600

    Добавили одну запись в базу данных

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -2,7 +2,7 @@
 	$mysqli = new MySQLi ("localhost", "root", "", "mybase");// должен предоставить хостер
 	$mysqli->query("SET NAMES 'utf8'");
 	
-	
-	
+	$success = $mysqli->query("INSERT INTO `users` (`login`, `password`, `reg_date`) VALUES ('123', '".md5("123")."', '".time ()."')");
+	echo $success;	
 	$mysqli->close();
 ?>
