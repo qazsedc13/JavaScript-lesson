44aeb9c5675d7ab7626c10f3bec804892cf6615d
qazsedc13 <bgnikkov@rambler.ru>
2016-01-28 11:37:31 +0600

    Установли куки и проверили установлена ли она

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,9 +1,8 @@
 ﻿<?php
-	$message = "Это самое простое сообщение в мире!";
-	$to = "qazsedc13@yandex.ru";
-	$from = "qazsedc13@yandex.com";
-	$subject = "Тема сообщения";
-	$subject = "=?utf-8?B?".base64_encode($subject)."?=";//тема передастся по спец. протоколу. для того чтобы mail.ru правильно обработал
-	$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";// очень важны для правильной обработки сообщения
-	mail ($to, $subject, $message, $headers);
+	//setcookie("num", 10, time() + 7);// добавили
+	echo $_COOKIE["num"];
+	if(isset($_COOKIE["num"])){
+		echo "Кука установлена";
+	} else
+		echo "Кука не установлена";
 ?>
