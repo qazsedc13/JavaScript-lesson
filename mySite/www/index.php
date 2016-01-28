3d2aa7984a0acd478fa53b7ee260adb75e8f3a9d
qazsedc13 <bgnikkov@rambler.ru>
2016-01-28 11:26:25 +0600

    Улучшили

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,6 +1,9 @@
 ﻿<?php
 	$message = "Это самое простое сообщение в мире!";
 	$to = "qazsedc13@yandex.ru";
+	$from = "qazsedc13@yandex.com";
 	$subject = "Тема сообщения";
-	mail ($to, $subject, $message);
+	$subject = "=?utf-8?B?".base64_encode($subject)."?=";//тема передастся по спец. протоколу. для того чтобы mail.ru правильно обработал
+	$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";// очень важны для правильной обработки сообщения
+	mail ($to, $subject, $message, $headers);
 ?>
