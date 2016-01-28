c12dbe1030d9ec24014372827e6c3cb59c7421ca
qazsedc13 <bgnikkov@rambler.ru>
2016-01-28 14:22:25 +0600

    Написали форму в html

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,9 +1,22 @@
-﻿<?php
-	session_start();
-	$num = (isset($_SESSION["num"])) ? $_SESSION["num"] : 0;
-	$num ++;
-	$_SESSION["num"] = $num;
-	echo "Пользователь обновил страничку $num раз";
-	
-	session_destroy ();	
-?>
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Обратная связь</title>
+</head>
+
+<body>
+	<h2>Форма обратной связи</h2>
+    <form name="feedback" action="" method="post">
+    	<label>От кого:</label><br />
+        <input type="text" name="from" /><br />
+        <label>Кому:</label><br />
+        <input type="text" name="to" /><br />
+        <label>Тема:</label><br />
+        <input type="text" name="subject" /><br />
+        <label>Сообщение:</label><br />
+        <textarea name="message" cols="30" rows="10"></textarea><br />
+        <input type="submit" name="send" value="Отправить" />
+    </form>
+</body>
+</html>
