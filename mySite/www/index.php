ba007575ec00d0881e81be2f5f95bc2e714f4bfe
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-28 20:14:29 +0600

    Добавили несколько записей в БД

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -4,5 +4,10 @@
 	
 	$success = $mysqli->query("INSERT INTO `users` (`login`, `password`, `reg_date`) VALUES ('123', '".md5("123")."', '".time ()."')");
 	echo $success;	
+	
+	for($i = 1; $i < 10; $i++){
+		$mysqli->query("INSERT INTO `users` (`login`, `password`, `reg_date`) VALUES ('$i', '".md5("$i")."', '".time ()."')");
+	}
+	
 	$mysqli->close();
 ?>
