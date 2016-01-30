316196343b7ce49a48d9172a6982edc3edf619c7
unknown <C:\Users\kolya\AppData\Roaming\The Bat!>
2016-01-30 12:36:11 +0600

    Вывели отдельно все id

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,16 +1,20 @@
 <?php
 	function printResult ($result_set) {
+		echo "Количество записей равно - ".$result_set->num_rows."<br />";
 		while (($row = $result_set->fetch_assoc()) != false) {
 			print_r ($row);
-			//echo $row("login");
 			echo "<br />";	
 		}	
-		echo "Количество записей равно - ".$result_set->num_rows."<br />------------------------------";
+		echo "<br />------------------------------<br />";
 	}
 	$mysqli = new MySQLi ("localhost", "root", "", "mybase");// должен предоставить хостер
 	$mysqli->query("SET NAMES 'utf8'");
 	
 	$result_set = $mysqli->query ("SELECT * FROM  `users`");
 	printResult ($result_set);
+	
+	$result_set = $mysqli->query ("SELECT `id` FROM  `users`");
+	printResult ($result_set);
+	
 	$mysqli->close();
 ?>
